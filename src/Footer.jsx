ffad63edc6728224756691ff1f61a21e753fda12
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2025-11-25 14:22:43 +1300

    fix whitespace before fullstop

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer style={{ marginTop: "2rem" }}>
-      <h3>Written by{" "} <a href="https://oweltonrosie.com" target="_blank" rel="noopener noreferrer">Oscar Welton-Rosie</a>{" "}. {" "}<a href="https://github.com/OWelton-Rosie/Cumulative-calculator" target="_blank" rel="noopener noreferrer">GitHub</a>.</h3>
+      <h3>Written by{" "} <a href="https://oweltonrosie.com" target="_blank" rel="noopener noreferrer">Oscar Welton-Rosie</a>. <a href="https://github.com/OWelton-Rosie/Cumulative-calculator" target="_blank" rel="noopener noreferrer">GitHub</a>.</h3>
     </footer>
   );
 }
